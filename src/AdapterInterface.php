078e1e898423138299f76cfe250b4c4481432513
Frank de Jonge <info@frenky.net>
2019-04-18 21:37:21 +0200

    Merge ReadInterface with the Adapter interface.

--- src/AdapterInterface.php
+++ src/AdapterInterface.php
@@ -2,7 +2,7 @@
 
 namespace League\Flysystem;
 
-interface AdapterInterface extends ReadInterface
+interface AdapterInterface
 {
     /**
      * @const  VISIBILITY_PUBLIC  public visibility
@@ -13,6 +13,88 @@ interface AdapterInterface extends ReadInterface
      * @const  VISIBILITY_PRIVATE  private visibility
      */
     const VISIBILITY_PRIVATE = 'private';
+
+    /**
+     * Check whether a file exists.
+     *
+     * @param string $path
+     *
+     * @return array|bool|null
+     */
+    public function has($path);
+
+    /**
+     * Read a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function read($path);
+
+    /**
+     * Read a file as a stream.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function readStream($path);
+
+    /**
+     * List contents of a directory.
+     *
+     * @param string $directory
+     * @param bool   $recursive
+     *
+     * @return array
+     */
+    public function listContents($directory = '', $recursive = false);
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMetadata($path);
+
+    /**
+     * Get the size of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getSize($path);
+
+    /**
+     * Get the mimetype of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMimetype($path);
+
+    /**
+     * Get the last modified time of a file as a timestamp.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getTimestamp($path);
+
+    /**
+     * Get the visibility of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getVisibility($path);
 
     /**
      * Write a new file.
